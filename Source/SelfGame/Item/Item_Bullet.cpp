dd5bba50b6225179da6fc74e531a4596e1fbeb23
dinoston <dinoston@gmail.com>
2025-10-19 21:19:27 +0900

    bullet아이템 픽업시 30개 플러스, 20개 추가 표시 로그

--- Source/SelfGame/Item/Item_Bullet.cpp
+++ Source/SelfGame/Item/Item_Bullet.cpp
@@ -2,4 +2,44 @@
 
 
 #include "Item_Bullet.h"
+#include "../Character/BaseCharacter.h"
+#include "../Weapon.h"
+#include "Engine/Engine.h"
+#include "Kismet/GameplayStatics.h"   
 
+
+void AItem_Bullet::ApplyTo(ABaseCharacter* Character)
+{
+    if (!Character || !Character->CurrentWeapon) return;
+
+    // 무기 탄약 보급
+    const int32 Added = Character->CurrentWeapon->AddAmmo(GiveAmount);
+
+    // 보급이 실제로 되었을 때만 재생 (가득 차 있으면 생략)
+    if (Added > 0 && PickupSound)
+    {
+        // 3D로 들리게: 아이템 위치에서 재생
+        UGameplayStatics::PlaySoundAtLocation(this, PickupSound, GetActorLocation());
+        // 2D UI처럼 전체에 들리게 하고 싶으면:
+        // UGameplayStatics::PlaySound2D(this, PickupSound);
+    }
+
+
+    // 화면 안내
+    if (GEngine)
+    {
+        if (Added > 0)
+        {
+            GEngine->AddOnScreenDebugMessage(-1, 1.5f, FColor::Cyan,
+                FString::Printf(TEXT("+%d Ammo  (%d/%d)"),
+                    Added,
+                    Character->CurrentWeapon->Ammo,
+                    Character->CurrentWeapon->MaxAmmo));
+        }
+        else
+        {
+            GEngine->AddOnScreenDebugMessage(-1, 1.5f, FColor::Yellow,
+                TEXT("Ammo is already full"));
+        }
+    }
+}
--- Source/SelfGame/Item/Item_Bullet.h
+++ Source/SelfGame/Item/Item_Bullet.h
@@ -14,4 +14,17 @@ class SELFGAME_API AItem_Bullet : public AItemBase
 {
 	GENERATED_BODY()
 	
+
+public:
+	// 한 번 먹을 때 채워줄 탄약 양
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Pickup")
+	int32 GiveAmount = 30;
+
+
+	//먹는 소리(에디터에서 사운드 큐/사운드 지정)
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Pickup")
+	USoundBase* PickupSound = nullptr;
+private:
+	virtual void ApplyTo(class ABaseCharacter* Character) override;
+
 };
